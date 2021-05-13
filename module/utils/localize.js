b9ac90781c805fd06a98cdd28efc2d4aa8102ce0
n0qtrnL <n0q@discordia.darkmaze.org>
2021-05-13 02:51:54 -0700

    further localization

--- module/utils/localize.js
+++ module/utils/localize.js
@@ -2,23 +2,23 @@
 const idx = {};
 
 idx.ability_short = {
-    str: "ACTOR.ability_short.str",
-    int: "ACTOR.ability_short.int",
-    wis: "ACTOR.ability_short.wis",
-    dex: "ACTOR.ability_short.dex",
-    con: "ACTOR.ability_short.con",
-    lks: "ACTOR.ability_short.lks",
-    cha: "ACTOR.ability_short.cha"
+    str: "HM.ability_short.str",
+    int: "HM.ability_short.int",
+    wis: "HM.ability_short.wis",
+    dex: "HM.ability_short.dex",
+    con: "HM.ability_short.con",
+    lks: "HM.ability_short.lks",
+    cha: "HM.ability_short.cha"
 },
 
 idx.ability = {
-    str: "ACTOR.str",
-    int: "ACTOR.int",
-    wis: "ACTOR.wis",
-    dex: "ACTOR.dex",
-    con: "ACTOR.con",
-    lks: "ACTOR.lks",
-    cha: "ACTOR.cha"
+    str: "HM.str",
+    int: "HM.int",
+    wis: "HM.wis",
+    dex: "HM.dex",
+    con: "HM.con",
+    lks: "HM.lks",
+    cha: "HM.cha"
 }
 
 export default idx;
