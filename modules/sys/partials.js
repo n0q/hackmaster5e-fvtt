e2c1dfbe693dadc1a7d33fa8deec3a9de60df80f
n0q <n0q@darkmaze.org>
2021-05-25 17:15:30 -0700

    adjusted armor data structure

--- modules/sys/partials.js
+++ modules/sys/partials.js
@@ -15,6 +15,8 @@ export default function preloadHandlebarsTemplates() {
         "systems/hackmaster5e/templates/actor/parts/setup/setup-classes.hbs",
         "systems/hackmaster5e/templates/actor/parts/setup/setup-profs.hbs",
         "systems/hackmaster5e/templates/actor/parts/inventory.hbs",
-        "systems/hackmaster5e/templates/actor/parts/skills.hbs"
+        "systems/hackmaster5e/templates/actor/parts/skills.hbs",
+        "systems/hackmaster5e/templates/item/parts/header-logistics.hbs",
+        "systems/hackmaster5e/templates/item/parts/logistics.hbs"
     ]);
 }
