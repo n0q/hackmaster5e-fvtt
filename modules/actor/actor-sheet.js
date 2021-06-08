2499a16f20560320a31b10127326f1d06aeea353
n0q <n0q@darkmaze.org>
2021-06-07 22:19:45 -0700

    placeholder spell sheet

--- modules/actor/actor-sheet.js
+++ modules/actor/actor-sheet.js
@@ -13,7 +13,7 @@ export class HackmasterActorSheet extends ActorSheet {
             template: "systems/hackmaster5e/templates/actor/actor-base.hbs",
             width: 820,
             height: 750,
-            tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "skills" }]
+            tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "magic" }]
         });
     }
 
