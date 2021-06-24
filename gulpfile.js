ec12e749bac30f48280a7180d3566d16afbf98ac
n0q <n0q@darkmaze.org>
2021-06-24 15:39:23 -0700

    lint

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,24 @@
 'use strict';
 const gulp = require('gulp');
 const sass = require('gulp-dart-sass');
+const cache = require('gulp-cached');
+const eslint = require('gulp-eslint');
 
 gulp.task('sass', function () {
-  return gulp.src('./scss/**/*.scss')
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('./styles'));
+    return gulp.src('./scss/**/*.scss')
+        .pipe(cache('sass'))
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(gulp.dest('./styles'));
 });
 
-gulp.task('watch', function () {
-  gulp.watch('./scss/**/*.scss', gulp.series('sass'));
+gulp.task('lint', function () {
+    return gulp.src(['hm.js', './modules/**/*.js'])
+        .pipe(cache('lint'))
+        .pipe(eslint())
+        .pipe(eslint.format());
 });
 
+gulp.task('default', function () {
+    gulp.watch('./scss/**/*.scss', gulp.series('sass'));
+    gulp.watch(['hm.js', './modules/**/*.js'], gulp.series('lint'));
+});
