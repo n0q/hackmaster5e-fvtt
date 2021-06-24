19f6d8d330e8445479debb516eb4eadf3c364af6
n0q <n0q@darkmaze.org>
2021-06-24 16:34:12 -0700

    ignore no-multi-spaces

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    'env': {
+        'es6': true,
+        'browser': true
+    },
+    'settings': {
+        'react': {'version': 'latest'}
+        },
+    'parserOptions': {
+        'ecmaVersion': 2020,
+        'sourceType': 'module',
+        'ecmaFeatures': {'jsx': true}
+    },
+    'extends': ['airbnb', '@typhonjs-fvtt/eslint-config-foundry.js'],
+    'rules': {
+        'indent': 'off',
+        'no-plusplus': 'off',
+        'no-multi-spaces': 'off',
+        'no-console': ['error', { allow: ['warn', 'error'] }],
+        'no-shadow': ['error', { 'builtinGlobals': true, 'hoist': 'all', 'allow': ['event'] }]
+    }
+}
