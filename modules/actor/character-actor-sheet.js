55d99eb7980f3cde0c5dad7bcb93a9168d904296
n0q <n0q@darkmaze.org>
2022-01-07 08:01:42 -0800

    default to skills on actor sheet

--- modules/actor/character-actor-sheet.js
+++ modules/actor/character-actor-sheet.js
@@ -15,8 +15,7 @@ export class HMCharacterActorSheet extends HMActorSheet {
             template: 'systems/hackmaster5e/templates/actor/actor-base.hbs',
             width: 820,
             height: 970,
-//            tabs: [{ navSelector: '.sheet-tabs', contentSelector: '.sheet-body', initial: 'skills' }]
-            tabs: [{ navSelector: '.sheet-tabs', contentSelector: '.sheet-body', initial: 'magic' }]
+            tabs: [{ navSelector: '.sheet-tabs', contentSelector: '.sheet-body', initial: 'skills' }]
         });
     }
 
