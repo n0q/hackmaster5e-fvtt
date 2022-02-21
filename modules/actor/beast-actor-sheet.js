a5f66fb56f4a0bfa3df644739702acdd2c5d65d5
n0q <n0q@darkmaze.org>
2022-02-21 03:44:34 -0800

    adjusted sheet mid-row appearance

--- modules/actor/beast-actor-sheet.js
+++ modules/actor/beast-actor-sheet.js
@@ -5,7 +5,7 @@ export class HMBeastActorSheet extends HMActorSheet {
     static get defaultOptions() {
         return mergeObject(super.defaultOptions, {
             classes: ['hackmaster', 'sheet', 'actor'],
-            width: 820,
+            width: 640,
             height: 970,
             tabs: [{ navSelector: '.sheet-tabs', contentSelector: '.sheet-body', initial: 'skills' }]
         });
