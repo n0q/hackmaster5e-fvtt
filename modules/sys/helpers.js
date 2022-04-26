4d17cbcf2ce9ba4db69a109f8c238d7b79eb722d
n0q <n0q@darkmaze.org>
2022-04-26 05:22:09 -0700

    cleanup

--- modules/sys/helpers.js
+++ modules/sys/helpers.js
@@ -1,12 +1,13 @@
-import LOGGER from "./logger.js";
-import idx from "./localize.js";
+/* global DOMPurify */
+import LOGGER from './logger.js';
+import { idx } from './localize.js';
 
 export default function registerHandlebarsHelpers() {
-    LOGGER.log("Calling Register Handlebars Helpers");
+    LOGGER.log('Calling Register Handlebars Helpers');
 
-    Handlebars.registerHelper('concat', function() {
-        var outStr = '';
-        for (var arg in arguments) {
+    Handlebars.registerHelper('concat', () => {
+        let outStr = '';
+        for (let arg in arguments) {
             if (typeof arguments[arg] != 'object') {
                 outStr += arguments[arg];
             }
@@ -14,28 +15,23 @@ export default function registerHandlebarsHelpers() {
         return outStr;
     });
 
-    Handlebars.registerHelper('toLowerCase', function(str) {
-        return str.toLowerCase();
-    });
+    Handlebars.registerHelper('toLowerCase', (str) => str.toLowerCase());
+    Handlebars.registerHelper('sanitize', (str) => DOMPurify.sanitize(str));
 
-    Handlebars.registerHelper('sanitize', function(str) {
-        return DOMPurify.sanitize(str);
-    });
-
-    Handlebars.registerHelper("findConfigValue", (obj, key) => {
+    Handlebars.registerHelper('findConfigValue', (obj, key) => {
         LOGGER.trace(`Calling findConfigValue Helper | Arg1:${obj} Arg2:${key}`);
         if (obj in idx) {
             return idx[obj][key];
         }
-        return "INVALID_KEY";
+        return 'INVALID_KEY';
     });
 
-    Handlebars.registerHelper("findConfigObj", (obj) => {
+    Handlebars.registerHelper('findConfigObj', (obj) => {
         LOGGER.trace(`Calling findConfigObj Helper | Arg1:${obj}`);
         if (obj in idx) {
             return idx[obj];
         }
-        return "INVALID_LIST";
+        return 'INVALID_LIST';
     });
 
     Handlebars.registerHelper('findBonus', (arg1, arg2, opts) => {
@@ -44,11 +40,8 @@ export default function registerHandlebarsHelpers() {
         return getProperty(vector, arg2);
     });
 
-    Handlebars.registerHelper('eq', (a, b) => { return a == b });
-
-    Handlebars.registerHelper("ishalf", (a, b)   => {
-        return a === Math.floor(b/2);
-    });
+    Handlebars.registerHelper('eq', (a, b) => a == b);
+    Handlebars.registerHelper('ishalf', (a, b) => a === Math.floor(b/2));
 
     Handlebars.registerHelper('pad', (arg1) => {
         let num = (arg1 || 0).toString();
@@ -56,8 +49,8 @@ export default function registerHandlebarsHelpers() {
         return num;
     });
 
-    Handlebars.registerHelper("repeat", function (count, opts) {
-        let str = "";
+    Handlebars.registerHelper('repeat', (count, opts) => {
+        let str = '';
 
         if (count) {
             for (let i = 0; i < count; i++) {
