35de0dd7972a99b2b0769c3e7e4e714879b3bd43
n0q <n0q@darkmaze.org>
2022-05-19 05:16:19 -0700

    actor cclass/race prep refactor

--- modules/item/race-item.js
+++ modules/item/race-item.js
@@ -4,21 +4,17 @@ import { HMItem } from './item.js';
 export class HMRaceItem extends HMItem {
     prepareBaseData() {
         super.prepareBaseData();
-        this._prepRace();
-    }
 
-    prepareDerivedData() {
-        super.prepareDerivedData();
-    }
-
-    _prepRace() {
         const {data} = this.data;
         const {bonus, scale} = data;
-
         const scaleTable = HMTABLES.scale;
         Object.keys(scale).forEach((key) => {
             const idx = data.scale[key];
             if (idx > 0) { bonus[key] = scaleTable[idx][key]; }
         });
+    }
+
+    prepareDerivedData() {
+        super.prepareDerivedData();
     }
 }
