bdda876ce925b7cdc85748701a2c2e6968f544d9
n0q <n0q@darkmaze.org>
2022-10-18 22:54:19 -0700

    slimmed actor sheet heights

--- modules/actor/character-actor-sheet.js
+++ modules/actor/character-actor-sheet.js
@@ -6,7 +6,7 @@ export class HMCharacterActorSheet extends HMActorSheet {
         return mergeObject(super.defaultOptions, {
             classes: ['hackmaster', 'sheet', 'actor'],
             width: 835,
-            height: 950,
+            height: 945,
             tabs: [{ navSelector: '.sheet-tabs', contentSelector: '.sheet-body', initial: 'skills' }],
         });
     }
