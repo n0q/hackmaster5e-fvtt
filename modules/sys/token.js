12b63307b3e0a3b4a130ef21bb21f27fa2f21e2a
n0q <n0q@darkmaze.org>
2022-10-30 11:28:29 -0700

    getReach() updates reachHint if it faults

--- modules/sys/token.js
+++ modules/sys/token.js
@@ -19,6 +19,7 @@ function getReach(actor) {
                 ?? weapons.find((a) => a.system.state === ITEM_STATE.EQUIPPED)
                 ?? weapons.find((a) => a.system.innate);
     if (!weapon) return [];
+    if (weapon.id !== reachHint) actor.setFlag(MODULE_ID, 'reachHint', weapon.id);
 
     const reach = (weapon.system.reach || 0) + (actor.system.bonus.total?.reach || 0);
     const distance = Math.max(reach, 0) + (game.canvas.scene.grid.distance / 2);
