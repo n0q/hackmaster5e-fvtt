cf2905d1ed2ee0e3e51d9b29f1d9563826763293
n0q <n0q@darkmaze.org>
2022-11-26 20:18:55 -0800

    casting fixes

--- modules/mgr/chatmgr.js
+++ modules/mgr/chatmgr.js
@@ -225,14 +225,15 @@ async function createSpellCard(dataset) {
         ? game.i18n.localize('HM.CHAT.cspell')
         : game.i18n.localize('HM.CHAT.mspell');
     const flavor = `${sLevel} ${game.i18n.localize('HM.level')} ${sType}`;
-    const rollContent = await roll.render({flavor});
+    const rollContent = roll ? await roll.render({flavor}) : undefined;
 
-    resp.total = roll.total;
+    roll ? resp.total = roll.total : resp.flavor = flavor;
     const template = resp.button === 'declare'
         ? 'systems/hackmaster5e/templates/chat/declare.hbs'
         : 'systems/hackmaster5e/templates/chat/spell.hbs';
-    const resultContent = await renderTemplate(template, dataset);
-    const content = resultContent + rollContent;
+
+    let content = await renderTemplate(template, dataset);
+    if (rollContent) content += rollContent;
     return {content, whisper, roll, flavor: caller.name};
 }
 
