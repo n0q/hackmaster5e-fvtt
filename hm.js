f71709f4b2508de28eedb8916a8a9688e903160e
n0q <n0q@darkmaze.org>
2022-12-02 01:04:35 -0800

    hooks cleanup

--- hm.js
+++ hm.js
@@ -1,3 +1,4 @@
+import { MODULE_ID } from './modules/sys/constants.js';
 import { HMActor } from './modules/actor/actor.js';
 import { HMActorFactory } from './modules/actor/actor-factory.js';
 import { HMBeastActorSheet } from './modules/actor/beast-actor-sheet.js';
@@ -12,22 +13,31 @@ import { HMClassItemSheet } from './modules/item/class-item-sheet.js';
 import { HMRaceItemSheet } from './modules/item/race-item-sheet.js';
 import { HMSpellItemSheet } from './modules/item/spell-item-sheet.js';
 import { HMWeaponItemSheet } from './modules/item/weapon-item-sheet.js';
-import { HMChatMgr } from './modules/mgr/chatmgr.js';
 import { HMCombat, HMCombatTracker } from './modules/sys/combat.js';
 import { HMDie } from './modules/sys/dice.js';
 import { HMMacro } from './modules/sys/macro.js';
 import { HMToken } from './modules/sys/token.js';
-import { HMSupport } from './modules/sys/support.js';
-import { HMStates, HMActiveEffect } from './modules/sys/effects.js';
-import { MODULE_ID } from './modules/sys/constants.js';
+import { HMActiveEffect } from './modules/sys/effects.js';
 import { registerSystemSettings } from './modules/sys/settings.js';
-
+import { registerHooks } from './modules/sys/hooks.js';
 import registerHandlebarsHelpers from './modules/sys/helpers.js';
 import preloadHandlebarsTemplates from './modules/sys/partials.js';
 
-Hooks.once('init', async () => {
-    game[MODULE_ID] = { HMActor, HMItem, HMWeaponItem, HMSpellItem };
+function registerSheets() {
+    Actors.unregisterSheet('core', ActorSheet);
+    Actors.registerSheet('hackmaster', HMCharacterActorSheet, {types: ['character'], makeDefault: true});
+    Actors.registerSheet('hackmaster', HMBeastActorSheet, {types: ['beast'], makeDefault: true});
 
+    Items.unregisterSheet('core', ItemSheet);
+    Items.registerSheet('hackmaster', HMItemSheet, {makeDefault: true});
+    Items.registerSheet('hackmaster', HMArmorItemSheet, {types: ['armor'], makeDefault: true});
+    Items.registerSheet('hackmaster', HMClassItemSheet, {types: ['cclass'], makeDefault: true});
+    Items.registerSheet('hackmaster', HMRaceItemSheet, {types: ['race'], makeDefault: true});
+    Items.registerSheet('hackmaster', HMSpellItemSheet, {types: ['spell'], makeDefault: true});
+    Items.registerSheet('hackmaster', HMWeaponItemSheet, {types: ['weapon'], makeDefault: true});
+}
+
+function registerConfig() {
     CONFIG.ActiveEffect.documentClass = HMActiveEffect;
     CONFIG.Actor.documentClass = HMActorFactory;
     CONFIG.Combat.documentClass = HMCombat;
@@ -53,56 +63,15 @@ Hooks.once('init', async () => {
             {urls: ['systems/hackmaster5e/styles/fonts/GenBkBasBI.woff2'], style: 'italic', weight: 700},
         ],
     };
+}
 
-    Actors.unregisterSheet('core', ActorSheet);
-    Actors.registerSheet('hackmaster', HMCharacterActorSheet, {types: ['character'], makeDefault: true});
-    Actors.registerSheet('hackmaster', HMBeastActorSheet, {types: ['beast'], makeDefault: true});
+Hooks.once('init', async () => {
+    game[MODULE_ID] = { HMActor, HMItem, HMWeaponItem, HMSpellItem };
 
-    Items.unregisterSheet('core', ItemSheet);
-    Items.registerSheet('hackmaster', HMItemSheet, {makeDefault: true});
-    Items.registerSheet('hackmaster', HMArmorItemSheet, {types: ['armor'], makeDefault: true});
-    Items.registerSheet('hackmaster', HMClassItemSheet, {types: ['cclass'], makeDefault: true});
-    Items.registerSheet('hackmaster', HMRaceItemSheet, {types: ['race'], makeDefault: true});
-    Items.registerSheet('hackmaster', HMSpellItemSheet, {types: ['spell'], makeDefault: true});
-    Items.registerSheet('hackmaster', HMWeaponItemSheet, {types: ['weapon'], makeDefault: true});
-
+    registerConfig();
+    registerSheets();
     registerHandlebarsHelpers();
     preloadHandlebarsTemplates();
     registerSystemSettings();
+    registerHooks();
 });
-
-Hooks.once('ready', async () => {
-    if (game.modules.get('_dev-mode')?.api?.getPackageDebugValue(MODULE_ID)) {
-        const tItem = game.items.contents.find((a) => a.name === 'test');
-        if (tItem) { tItem.sheet.render(true); }
-        const tActor = game.actors.contents.find((a) => a.name === 'test');
-        if (tActor) { tActor.sheet.render(true); }
-    }
-
-    if (!game.user.isGM) return;
-    const folderName = game.i18n.localize('HM.sys.folders.skillmacro');
-    let f = game.folders.find((a) => a.type === 'Macro' && a.name === folderName);
-    if (!f) f = await Folder.create({type: 'Macro', name: folderName, parent: null});
-});
-
-Hooks.once('setup', HMStates.setupStatusEffects);
-Hooks.once('devModeReady', HMSupport.devModeReady);
-Hooks.once('dragRuler.ready', HMSupport.dragRuler_ready);
-Hooks.on('applyActiveEffect', HMActiveEffect.applyActiveEffect);
-Hooks.on('createActiveEffect', HMActiveEffect.createActiveEffect);
-Hooks.on('createActor', HMActor.createActor);
-Hooks.on('createToken', HMActor.createToken);
-Hooks.on('hoverToken', (token, state) => token.drawReach(state));
-Hooks.on('createItem', HMItem.createItem);
-Hooks.on('updateItem', HMWeaponItem.updateItem);
-Hooks.on('renderChatMessage', HMChatMgr.renderChatMessage);
-Hooks.on('updateCombat', HMCombat.updateCombat);
-Hooks.on('createCombatant', HMCombat.createCombatant);
-Hooks.on('deleteCombatant', HMCombat.deleteCombatant);
-Hooks.on('preDeleteCombat', HMCombat.preDeleteCombat);
-Hooks.on('deleteCombat', () => canvas.tokens.placeables.forEach((t) => t.drawReach()));
-Hooks.on('renderCombatTracker', HMCombatTracker.renderCombatTracker);
-Hooks.on('renderSceneControls', HMToken.renderSceneControls);
-Hooks.on('getSceneControlButtons', HMToken.getSceneControlButtons);
-Hooks.on('hotbarDrop', HMMacro.hotbarDrop);
-// Hooks.on('diceSoNiceRollStart', HMSupport.diceSoNiceRollStart);
