e6b57b3ec0ae1aa6e7d48c0d36769faff19a631f
n0q <n0q@darkmaze.org>
2022-12-22 17:01:30 -0800

    moved hooks out of HMActor class

---
+++ modules/hooks/actor-hooks.js
@@ -0,0 +1,18 @@
+export class HMActorHooks {
+    static async createActor(actor, _options, userId) {
+        if (game.user.id !== userId) return;
+        if (actor.items.size || actor.type === 'beast') return;
+
+        const skillPack = game.packs.get('hackmaster5e.uskills');
+        const itemList = await skillPack.getDocuments();
+
+        const innatePack = game.packs.get('hackmaster5e.hmbinnate');
+        const innateIndex = await innatePack.getIndex();
+
+        const itemId = innateIndex.getName('Unarmed')._id;
+        const unarmed = await innatePack.getDocument(itemId);
+        itemList.push(unarmed);
+
+        await actor.createEmbeddedDocuments('Item', itemList);
+    }
+}
