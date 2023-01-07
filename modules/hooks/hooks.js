f4c352af7c3cbf8b60e1f3a2c4fa9249f2cbf40b
n0q <n0q@darkmaze.org>
2023-01-07 03:44:19 -0800

    support hooks cleanup

--- modules/hooks/hooks.js
+++ modules/hooks/hooks.js
@@ -1,34 +1,20 @@
 import { MODULE_ID, SYSTEM_SOCKET } from '../tables/constants.js';
 import { HMActiveEffectHooks } from './effect-hooks.js';
-import { HMSupport } from '../sys/support.js';
 import { HMActorHooks } from './actor-hooks.js';
 import { HMChatHooks } from './chat-hooks.js';
+import { HMCombat, HMCombatTracker } from '../sys/combat.js';
 import { HMItemHooks } from './item-hooks.js';
 import { HMMacroHooks } from './macro-hooks.js';
 import { HMSceneControlHooks } from './scene-hooks.js';
+import { HMSupportHooks } from './support-hooks.js';
 import { HMTokenHooks } from './token-hooks.js';
-import { HMCombat, HMCombatTracker } from '../sys/combat.js';
 import { handleSocketEvent } from '../sys/sockets.js';
-
-async function ready() {
-    if (game.modules.get('_dev-mode')?.api?.getPackageDebugValue(MODULE_ID)) {
-        const tItem = game.items.contents.find((a) => a.name === 'test');
-        if (tItem) { tItem.sheet.render(true); }
-        const tActor = game.actors.contents.find((a) => a.name === 'test');
-        if (tActor) { tActor.sheet.render(true); }
-    }
-
-    if (!game.user.isGM) return;
-    const folderName = game.i18n.localize('HM.sys.folders.skillmacro');
-    let f = game.folders.find((a) => a.type === 'Macro' && a.name === folderName);
-    if (!f) f = await Folder.create({type: 'Macro', name: folderName, parent: null});
-}
 
 export const registerHooks = () => {
     Hooks.once('ready', ready);
     Hooks.once('setup', HMActiveEffectHooks.setupStatusEffects);
-    Hooks.once('devModeReady', HMSupport.devModeReady);
-    Hooks.once('dragRuler.ready', HMSupport.dragRuler_ready);
+    Hooks.once('devModeReady', HMSupportHooks.devModeReady);
+    Hooks.once('dragRuler.ready', HMSupportHooks.dragRuler_ready);
     Hooks.on('applyActiveEffect', HMActiveEffectHooks.applyActiveEffect);
     Hooks.on('createActiveEffect', HMActiveEffectHooks.createActiveEffect);
     Hooks.on('deleteActiveEffect', HMActiveEffectHooks.deleteActiveEffect);
@@ -50,3 +36,17 @@ export const registerHooks = () => {
     Hooks.on('hotbarDrop', HMMacroHooks.hotbarDrop);
     game.socket.on(SYSTEM_SOCKET, handleSocketEvent);
 };
+
+async function ready() {
+    if (game.modules.get('_dev-mode')?.api?.getPackageDebugValue(MODULE_ID)) {
+        const tItem = game.items.contents.find((a) => a.name === 'test');
+        if (tItem) { tItem.sheet.render(true); }
+        const tActor = game.actors.contents.find((a) => a.name === 'test');
+        if (tActor) { tActor.sheet.render(true); }
+    }
+
+    if (!game.user.isGM) return;
+    const folderName = game.i18n.localize('HM.sys.folders.skillmacro');
+    let f = game.folders.find((a) => a.type === 'Macro' && a.name === folderName);
+    if (!f) f = await Folder.create({type: 'Macro', name: folderName, parent: null});
+}
