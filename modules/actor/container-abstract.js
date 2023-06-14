773bcd8f2abba78b27cc8ef77dd13feba1069ada
n0q <n0q@darkmaze.org>
2023-06-14 10:10:27 -0700

    HMContainerAbstract for container item operations

---
+++ modules/actor/container-abstract.js
@@ -0,0 +1,28 @@
+export class HMItemContainer extends foundry.abstract.DataModel {
+    static defineSchema() {
+        const {fields} = foundry.data;
+        return {
+            actor: new fields.ObjectField({blank: false, required: true, readonly: true}),
+        };
+    }
+
+    get items() {
+        const icItems = new foundry.utils.Collection(this.actor.items.entries());
+        const stack = icItems.filter((item) => item.system?.container?.enabled);
+
+        while (stack.length) {
+            const container = stack.pop();
+            container.items.forEach((i) => {
+                icItems.set(i.id, i);
+                if (i.system?.container?.enabled) stack.push(i);
+            });
+        }
+        return icItems;
+    }
+
+    get itemTypes() {
+        const types = Object.fromEntries(game.documentTypes.Item.map((t) => [t, []]));
+        this.items.contents.forEach((item) => types[item.type].push(item));
+        return types;
+    }
+}
