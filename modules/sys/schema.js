b5754653e80fd61d26f9ee5c221976531127d0e2
n0q <n0q@darkmaze.org>
2024-04-06 15:08:06 -0700

    migrated Race items to DataModel

--- modules/sys/schema.js
+++ modules/sys/schema.js
@@ -1,7 +1,9 @@
 import { HMCurrencySchema } from '../item/schema/currency-item.schema.js';
-import { HMWoundSchema }    from '../item/schema/wound-item-schema.js';
+import { HMRaceSchema } from '../item/schema/race-item-schema.js';
+import { HMWoundSchema } from '../item/schema/wound-item-schema.js';
 
 export const registerSchema = () => {
     CONFIG.Item.dataModels.currency = HMCurrencySchema;
+    CONFIG.Item.dataModels.race = HMRaceSchema;
     CONFIG.Item.dataModels.wound = HMWoundSchema;
 };
