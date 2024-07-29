0eb87b2117bf2464c6bce76dee99c24440a9b9f4
n0q <n0q@darkmaze.org>
2024-07-28 17:50:17 -0700

    moved dsn support to its own file

--- modules/hooks/support-hooks.js
+++ modules/hooks/support-hooks.js
@@ -2,7 +2,7 @@
 /* eslint max-classes-per-file: 0 */
 /* eslint class-methods-use-this: 0 */
 import { SYSTEM_ID } from '../tables/constants.js';
-import { HMDie } from '../sys/dice.js';
+import { diceSoNiceRollStartHandler } from '../integrations/dice-so-nice.js';
 import { tokenHPAttribute, HM_ER_SPEED } from '../integrations/elevationruler.js';
 
 export class HMSupportHooks {
@@ -18,44 +18,8 @@ export class HMSupportHooks {
         registerPackageDebugFlag(SYSTEM_ID);
     }
 
-    static async diceSoNiceRollStart(_, context) {
-        // eslint-disable-next-line no-shadow
-        const dsnDecay = (terms, dsnTerms, newTerms=[], r=5) => {
-            if (r < 0) return false;
-
-            terms.forEach((term, i) => {
-                const dsnTerm = dsnTerms[i];
-                if (term.rolls) {
-                    term.rolls.forEach((rolls, j) => {
-                        dsnDecay(term.rolls[j].terms, dsnTerm.rolls[j].terms, newTerms, r - 1);
-                    });
-                }
-
-                dsnTerm.results = foundry.utils.deepClone(term.results);
-
-                if (!dsnTerm.isDeterministic && dsnTerm.faces >= 20) {
-                    const biasIdx = dsnTerm.results.findIndex((x) => x.bias);
-                    if (biasIdx !== -1) {
-                        const results = dsnTerm.results.splice(biasIdx);
-                        const faces = results[0].faces ? results[0].faces : dsnTerm.faces;
-                        const newTerm = new HMDie({faces, results});
-                        newTerms.push(newTerm);
-                    }
-                }
-            });
-
-            if (r === 5) return dsnTerms.concat(newTerms);
-            return dsnTerms;
-        };
-
-        const dsnRoll = context.roll.clone();
-
-        const dsnTerms = Roll.simplifyTerms(dsnRoll.terms);
-        const srcTerms = Roll.simplifyTerms(context.roll.terms);
-        dsnRoll.terms = dsnDecay(srcTerms, dsnTerms);
-
-        dsnRoll.ghost = context.roll.ghost;
-        context.dsnRoll = dsnRoll;
+    static diceSoNiceRollStart(...args) {
+        return diceSoNiceRollStartHandler(...args);
     }
 
     static async dragRuler_ready(SpeedProvider) {
