d6951eb5751c9803f7bd0e1c9e69047f77791d75
n0q <n0q@darkmaze.org>
2025-07-22 13:52:40 -0700

    findConfigValue accepts a path string for arbitrary nesting

--- modules/sys/helpers.js
+++ modules/sys/helpers.js
@@ -39,7 +39,7 @@ export const registerHandlebarsHelpers = () => {
     });
 
     Handlebars.registerHelper('toggleSwitch', (id, opts) => {
-        const {visibleItemId} = opts.data.root.document.sheet;
+        const { visibleItemId } = opts.data.root.document.sheet;
         let visible = !!visibleItemId[id];
         if (!!opts.hash.invert) visible = !visible;
         if (!visible) return 'hide';
@@ -52,23 +52,30 @@ export const registerHandlebarsHelpers = () => {
         return 'INVALID_KEY';
     });
 
-    Handlebars.registerHelper('findConfigValue', (obj, key) => {
-        if (obj in idx) return idx[obj][key];
-        return 'INVALID_KEY';
+    /**
+     * @param {string} pathString - Path to container object 'foo.bar.baz'
+     * @param {string|number} key - Final key to retrieve from idx;
+     * @returns {any|string} Found value, or 'INVALID_KEY' if not found.
+     */
+    Handlebars.registerHelper('findConfigValue', (pathString, key) => {
+        const pathParts = pathString.split('.');
+        const idxChildObj = pathParts.reduce((currentLevel, part) => currentLevel?.[part], idx);
+        const finalValue = idxChildObj?.[key];
+        return finalValue === undefined ? 'INVALID_KEY' : finalValue;
     });
 
     Handlebars.registerHelper('findConfigObj', (obj, opts) => {
-        const {omit} = opts.hash;
+        const { omit } = opts.hash;
         if (obj in idx) {
             if (!omit) return idx[obj];
-            const {[omit]: _, ...filteredObj} = idx[obj];
+            const { [omit]: _, ...filteredObj } = idx[obj];
             return filteredObj;
         }
         return 'INVALID_KEY';
     });
 
     Handlebars.registerHelper('findBonus', (arg1, arg2, opts) => {
-        const {bonus} = opts.data.root.actor.system;
+        const { bonus } = opts.data.root.actor.system;
         const vector = bonus[arg1];
         return foundry.utils.getProperty(vector, arg2);
     });
@@ -77,25 +84,24 @@ export const registerHandlebarsHelpers = () => {
         const cross = Array.isArray(obj1) ? obj1 : Object.values(obj1);
         const label = idx[obj2];
 
-        return {...cross.map((x, i) => `${game.i18n.localize(label[i])} (${x} ${suffix})`)};
+        return { ...cross.map((x, i) => `${game.i18n.localize(label[i])} (${x} ${suffix})`) };
     });
 
-    Handlebars.registerHelper('mergeObjs', (obj1, obj2) => ({...obj1, ...obj2}));
+    Handlebars.registerHelper('mergeObjs', (obj1, obj2) => ({ ...obj1, ...obj2 }));
 
     Handlebars.registerHelper('isGM', () => game.user.isGM);
     Handlebars.registerHelper('getFlag', (scope, key, opts) => opts.data.root.actor.getFlag(scope, key));
     Handlebars.registerHelper('getSetting', (scope, key) => game.settings.get(scope, key));
-    Handlebars.registerHelper('eq', (a, b) => a == b);
-    Handlebars.registerHelper('neq', (a, b) => a != b);
+    Handlebars.registerHelper('eq', (a, b) => a == b); // eslint-disable-line eqeqeq
+    Handlebars.registerHelper('neq', (a, b) => a != b); // eslint-disable-line eqeqeq
     Handlebars.registerHelper('ceil', (a) => Math.ceil(a));
-
     Handlebars.registerHelper('isEven', (a) => (((a % 2) + 2) % 2));
 
-    Handlebars.registerHelper('isHalf', (a, b, opts) => {
-        return opts.hash.ceil
-            ? a === Math.ceil(b/2)
-            : a === Math.floor(b/2);
-    });
+    Handlebars.registerHelper('isHalf', (a, b, opts) => (
+        opts.hash.ceil
+            ? a === Math.ceil(b / 2)
+            : a === Math.floor(b / 2)
+    ));
 
     Handlebars.registerHelper('pad', (arg1) => {
         let num = (arg1 || 0).toString();
@@ -109,7 +115,7 @@ export const registerHandlebarsHelpers = () => {
     });
 
     Handlebars.registerHelper('getValidContainers', (selectObj, thisId, opts) => {
-        const {containerMap} = opts.data.root.actor;
+        const { containerMap } = opts.data.root.actor;
         const blacklist = containerMap.get(thisId) ?? [];
 
         const srcKeys = Object.keys(selectObj);
@@ -125,13 +131,13 @@ export const registerHandlebarsHelpers = () => {
         let sortedKeys = Object.keys(obj2).sort((a, b) => obj2[a][key] - obj2[b][key]);
         if (!!opts.hash.reverse) sortedKeys = sortedKeys.reverse();
 
-        let sortedObj = {};
+        const sortedObj = {};
         sortedKeys.forEach((k) => sortedObj[k] = obj1[k]);
         return sortedObj;
     });
 
     Handlebars.registerHelper('itemSort', (itemTypes) => {
-        const {currency, weapon, armor} = itemTypes;
+        const { currency, weapon, armor } = itemTypes;
 
         const compareNames = (a, b) => a.name.localeCompare(b.name);
 
@@ -151,7 +157,7 @@ export const registerHandlebarsHelpers = () => {
     });
 
     Handlebars.registerHelper('delete', (arg1, arg2) => {
-        const {[arg2]: _, ...deleted} = arg1;
+        const { [arg2]: _, ...deleted } = arg1;
         return deleted;
     });
 
@@ -160,7 +166,7 @@ export const registerHandlebarsHelpers = () => {
 
         if (count) {
             for (let i = 0; i < count; i++) {
-                const data = {index: i};
+                const data = { index: i };
                 str += opts.fn(this, data);
             }
         } else str = opts.inverse(this);
