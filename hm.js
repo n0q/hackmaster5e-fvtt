e4cda6ddc85a4a432aad43e7b6005c86e2b7973b
n0q <n0q@darkmaze.org>
2025-08-03 05:34:44 -0700

    init cleanup

--- hm.js
+++ hm.js
@@ -1,63 +1,63 @@
-import { SYSTEM_ID } from './modules/tables/constants.js';
-import { HMActor } from './modules/actor/actor.js';
-import { HMActorFactory } from './modules/actor/actor-factory.js';
-import { HMBeastActorSheet } from './modules/actor/beast-actor-sheet.js';
-import { HMCharacterActorSheet } from './modules/actor/character-actor-sheet.js';
-import { HMItem } from './modules/item/item.js';
-import { HMWeaponItem } from './modules/item/weapon-item.js';
-import { HMSpellItem } from './modules/item/spell-item.js';
-import { HMItemFactory } from './modules/item/item-factory.js';
-import { HMItemSheet } from './modules/item/sheets/item-sheet.js';
-import { HMArmorItemSheet } from './modules/item/sheets/armor-item-sheet.js';
-import { HMClassItemSheet } from './modules/item/sheets/class-item-sheet.js';
-import { HMCurrencyItemSheet } from './modules/item/sheets/currency-item-sheet.js';
-import { HMProficiencyItemSheet } from './modules/item/sheets/proficiency-item-sheet.js';
-import { HMRaceItemSheet } from './modules/item/sheets/race-item-sheet.js';
-import { HMSpellItemSheet } from './modules/item/sheets/spell-item-sheet.js';
-import { HMTalentItemSheet } from './modules/item/sheets/talent-item-sheet.js';
-import { HMWeaponItemSheet } from './modules/item/sheets/weapon-item-sheet.js';
-import { HMWoundItemSheet } from './modules/item/sheets/wound-item-sheet.js';
-import { HMCombat } from './modules/combat/combat.js';
-import { HMCombatTracker } from './modules/combat/combat-tracker.js';
-import { HMDie } from './modules/sys/dice.js';
-import { HMToken } from './modules/sys/token.js';
-import { HMTokenRuler } from './modules/sys/token-ruler.js';
-import { HMActiveEffect } from './modules/sys/effects.js';
-import { registerSystemSettings } from './modules/sys/settings.js';
-import { registerHooks } from './modules/hooks/hooks.js';
-import { registerHandlebarsHelpers } from './modules/sys/helpers.js';
-import { preloadHandlebarsTemplates } from './modules/sys/templates.js';
+import { SYSTEM_ID, systemPath } from "./modules/tables/constants.js";
+import { HMActor } from "./modules/actor/actor.js";
+import { HMActorFactory } from "./modules/actor/actor-factory.js";
+import { HMBeastActorSheet } from "./modules/actor/beast-actor-sheet.js";
+import { HMCharacterActorSheet } from "./modules/actor/character-actor-sheet.js";
+import { HMItem } from "./modules/item/item.js";
+import { HMWeaponItem } from "./modules/item/weapon-item.js";
+import { HMSpellItem } from "./modules/item/spell-item.js";
+import { HMItemFactory } from "./modules/item/item-factory.js";
+import { HMItemSheet } from "./modules/item/sheets/item-sheet.js";
+import { HMArmorItemSheet } from "./modules/item/sheets/armor-item-sheet.js";
+import { HMClassItemSheet } from "./modules/item/sheets/class-item-sheet.js";
+import { HMCurrencyItemSheet } from "./modules/item/sheets/currency-item-sheet.js";
+import { HMProficiencyItemSheet } from "./modules/item/sheets/proficiency-item-sheet.js";
+import { HMRaceItemSheet } from "./modules/item/sheets/race-item-sheet.js";
+import { HMSpellItemSheet } from "./modules/item/sheets/spell-item-sheet.js";
+import { HMTalentItemSheet } from "./modules/item/sheets/talent-item-sheet.js";
+import { HMWeaponItemSheet } from "./modules/item/sheets/weapon-item-sheet.js";
+import { HMWoundItemSheet } from "./modules/item/sheets/wound-item-sheet.js";
+import { HMCombat } from "./modules/combat/combat.js";
+import { HMCombatTracker } from "./modules/combat/combat-tracker.js";
+import { HMDie } from "./modules/sys/dice.js";
+import { HMToken } from "./modules/sys/token.js";
+import { HMTokenRuler } from "./modules/sys/token-ruler.js";
+import { HMActiveEffect } from "./modules/sys/effects.js";
+import { registerSystemSettings } from "./modules/sys/settings.js";
+import { registerHooks } from "./modules/hooks/hooks.js";
+import { registerHandlebarsHelpers } from "./modules/sys/helpers.js";
+import { preloadHandlebarsTemplates } from "./modules/sys/templates.js";
 import gsap, { PixiPlugin } from '/scripts/greensock/esm/all.js'; // eslint-disable-line
-import { registerSchema } from './modules/sys/schema.js';
-import { migrateData } from './modules/sys/migration.js';
+import { registerSchema } from "./modules/sys/schema.js";
+import { migrateData } from "./modules/sys/migration.js";
 
 function registerSheets() {
     const ActorsCollection = foundry.documents.collections.Actors;
     const ItemsCollection = foundry.documents.collections.Items;
 
-    ActorsCollection.unregisterSheet('core', foundry.appv1.sheets.ActorSheet);
-    ItemsCollection.unregisterSheet('core', foundry.appv1.sheets.ItemSheet);
+    ActorsCollection.unregisterSheet("core", foundry.appv1.sheets.ActorSheet);
+    ItemsCollection.unregisterSheet("core", foundry.appv1.sheets.ItemSheet);
 
     const actorSheets = [
-        { sheetClass: HMCharacterActorSheet, types: ['character'] },
-        { sheetClass: HMBeastActorSheet, types: ['beast'] },
+        { sheetClass: HMCharacterActorSheet, types: ["character"] },
+        { sheetClass: HMBeastActorSheet, types: ["beast"] },
     ];
 
     const itemSheets = [
         { sheetClass: HMItemSheet },
-        { sheetClass: HMArmorItemSheet, types: ['armor'] },
-        { sheetClass: HMClassItemSheet, types: ['cclass'] },
-        { sheetClass: HMCurrencyItemSheet, types: ['currency'] },
-        { sheetClass: HMProficiencyItemSheet, types: ['proficiency'] },
-        { sheetClass: HMRaceItemSheet, types: ['race'] },
-        { sheetClass: HMSpellItemSheet, types: ['spell'] },
-        { sheetClass: HMTalentItemSheet, types: ['talent'] },
-        { sheetClass: HMWeaponItemSheet, types: ['weapon'] },
-        { sheetClass: HMWoundItemSheet, types: ['wound'] },
+        { sheetClass: HMArmorItemSheet, types: ["armor"] },
+        { sheetClass: HMClassItemSheet, types: ["cclass"] },
+        { sheetClass: HMCurrencyItemSheet, types: ["currency"] },
+        { sheetClass: HMProficiencyItemSheet, types: ["proficiency"] },
+        { sheetClass: HMRaceItemSheet, types: ["race"] },
+        { sheetClass: HMSpellItemSheet, types: ["spell"] },
+        { sheetClass: HMTalentItemSheet, types: ["talent"] },
+        { sheetClass: HMWeaponItemSheet, types: ["weapon"] },
+        { sheetClass: HMWoundItemSheet, types: ["wound"] },
     ];
 
     actorSheets.forEach(({ sheetClass, types }) => {
-        ActorsCollection.registerSheet('hackmaster', sheetClass, { types, makeDefault: true });
+        ActorsCollection.registerSheet("hackmaster", sheetClass, { types, makeDefault: true });
     });
 
     itemSheets.forEach(({ sheetClass, types }) => {
@@ -65,37 +65,33 @@ function registerSheets() {
         if (types) {
             options.types = types;
         }
-        ItemsCollection.registerSheet('hackmaster', sheetClass, options);
+        ItemsCollection.registerSheet("hackmaster", sheetClass, options);
     });
 }
 
 function registerConfig() {
     CONFIG.ActiveEffect.documentClass = HMActiveEffect;
-    CONFIG.ActiveEffect.legacyTransferral = false;
     CONFIG.Actor.documentClass = HMActorFactory;
     CONFIG.Combat.documentClass = HMCombat;
     CONFIG.Item.documentClass = HMItemFactory;
 
     CONFIG.Dice.terms.d = HMDie;
-    const diceTypesIdx = CONFIG.Dice.types.findIndex((x) => x.DENOMINATION === 'd');
-    diceTypesIdx > -1
-        ? CONFIG.Dice.types[diceTypesIdx] = HMDie
-        : CONFIG.Dice.types.push(HMDie);
+    CONFIG.Dice.types = CONFIG.Dice.types.map(cls => cls.DENOMINATION === "d" ? HMDie : cls);
 
     CONFIG.Token.objectClass = HMToken;
     CONFIG.Token.rulerClass = HMTokenRuler;
     const { walk, displace } = CONFIG.Token.movement.actions;
     CONFIG.Token.movement.actions = { displace, walk };
     CONFIG.ui.combat = HMCombatTracker;
-    CONFIG.canvasTextStyle.fontFamily = 'Gentium';
+    CONFIG.canvasTextStyle.fontFamily = "Gentium";
 
     CONFIG.fontDefinitions.Gentium = {
         editor: true,
         fonts: [
-            { urls: ['systems/hackmaster5e/styles/fonts/GenBkBasR.woff2'] },
-            { urls: ['systems/hackmaster5e/styles/fonts/GenBkBasB.woff2'], weight: 700 },
-            { urls: ['systems/hackmaster5e/styles/fonts/GenBkBasI.woff2'], style: 'italic' },
-            { urls: ['systems/hackmaster5e/styles/fonts/GenBkBasBI.woff2'], style: 'italic', weight: 700 },
+            { urls: ["systems/hackmaster5e/styles/fonts/GenBkBasR.woff2"] },
+            { urls: ["systems/hackmaster5e/styles/fonts/GenBkBasB.woff2"], weight: 700 },
+            { urls: ["systems/hackmaster5e/styles/fonts/GenBkBasI.woff2"], style: "italic" },
+            { urls: ["systems/hackmaster5e/styles/fonts/GenBkBasBI.woff2"], style: "italic", weight: 700 },
         ],
     };
 
@@ -109,7 +105,7 @@ function registerGsapPlugins() {
     game.gsap = gsap;
 }
 
-Hooks.once('init', async () => {
+Hooks.once("init", async () => {
     game[SYSTEM_ID] = { HMActor, HMItem, HMWeaponItem, HMSpellItem };
     registerConfig();
     registerSchema();
@@ -121,6 +117,6 @@ Hooks.once('init', async () => {
     registerHooks();
 });
 
-Hooks.once('ready', () => {
+Hooks.once("ready", () => {
     migrateData();
 });
